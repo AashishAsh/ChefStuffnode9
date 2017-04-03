ed3ddaae4ca4d5005eb61d286137573d45197077
aashishash <abhiash2050@gmail.com>
2017-04-02 19:50:44 -0500

    tomcat8

--- cookbooks/tomcat8/attributes/default.rb
+++ cookbooks/tomcat8/attributes/default.rb
@@ -17,11 +17,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['tomcat8']['download_url'] = "http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz"
-default['tomcat8']['install_location'] = '/var/tomcat8'
-default['tomcat8']['port'] = 8080
-default['tomcat8']['ssl_port'] = 8443
-default['tomcat8']['ajp_port'] = 8009
-default['tomcat8']['java_options'] = "-Xmx128M"
-default['tomcat8']['tomcat_user'] = "root"
-default['tomcat8']['autostart'] = "true"
+default['tomcat8']['download_url'] = "http:GREENSKY SERVER LINK PASTE HERE tomcat-8.0.33.tar.gz"
+default['tomcat8']['install_location'] = '/opt/greensky'
+#default['tomcat8']['port'] = 8080
+#default['tomcat8']['ssl_port'] = 8443
+#default['tomcat8']['ajp_port'] = 8009
+#default['tomcat8']['java_options'] = "-Xmx128M"
+#default['tomcat8']['tomcat_user'] = "root"
+default['tomcat8']['tomcat_user'] = "tomcat"
+#default['tomcat8']['autostart'] = "true"
